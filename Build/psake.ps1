7a2e5478c82a44c415a89bf9583c7a3e09ea6680
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-08 17:34:08 +0100

    Fix for module path

--- Build/psake.ps1
+++ Build/psake.ps1
@@ -35,7 +35,7 @@ Task Test -Depends Init  {
     "`n`tSTATUS: Testing with PowerShell $PSVersion"
 
     # Gather test results. Store them in a variable and file
-    $env:PSModulePath += "$env:BHPSModulePath"
+    $env:PSModulePath += ";$ProjectRoot"
     $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -CodeCoverage "$env:BHPSModulePath\$env:BHProjectName.psm1"
 
     # In Appveyor?  Upload our tests! #Abstract this into a function?
