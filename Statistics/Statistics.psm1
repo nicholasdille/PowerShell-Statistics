5c4caa7d75d44c1ca68aa21e2dc6b7a08b48aa81
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-20 15:39:55 +0100

    Fixes notepad to pop up with format files

--- Statistics/Statistics.psm1
+++ Statistics/Statistics.psm1
@@ -8,6 +8,7 @@ $scriptBody = "using module '$PSScriptRoot\HistogramBar.psm1'"
 $script = [ScriptBlock]::Create($scriptBody)
 . $script
 
-Get-ChildItem -Path "$PSScriptRoot" -Filter '*.ps1' | ForEach-Object {
-    . "$($_.FullName)"
-}
+. "$PSScriptRoot\Get-Histogram.ps1"
+. "$PSScriptRoot\Add-Bar.ps1"
+. "$PSScriptRoot\Measure-Object.ps1"
+. "$PSScriptRoot\Show-Measurement.ps1"
