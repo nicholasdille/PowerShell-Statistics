cf2ec915f17a48138706ce94b23916a7f1f99137
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-28 12:10:40 +0200

    Loading of new cmdlet

--- Statistics/Statistics.psm1
+++ Statistics/Statistics.psm1
@@ -1,4 +1,5 @@
-﻿. "$PSScriptRoot\Get-Histogram.ps1"
+﻿. "$PSScriptRoot\ConvertFrom-PrimitiveType.ps1"
+. "$PSScriptRoot\Get-Histogram.ps1"
 . "$PSScriptRoot\Add-Bar.ps1"
 . "$PSScriptRoot\Measure-Object.ps1"
 . "$PSScriptRoot\Show-Measurement.ps1"
