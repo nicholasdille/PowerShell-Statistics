3cf81ee598faf534fe9d00facb7e4c1398022c2d
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 11:45:52 +0200

    Added tests for Expand-DateTime

--- Tests/Expand-DateTime.Tests.ps1
+++ Tests/Expand-DateTime.Tests.ps1
@@ -1,5 +1,19 @@
 Import-Module -Name Statistics -Force
 
 Describe 'Expand-DateTime' {
-    It '' {}
+    It 'Works on property implicitly' {
+        { Get-Counter -Counter '\Processor(_Total)\% Processor Time' | Expand-DateTime } | Should Not Throw
+    }
+    It 'Works on property explicitly' {
+        { Get-Counter -Counter '\Processor(_Total)\% Processor Time' | Expand-DateTime -Property Timestamp } | Should Not Throw
+    }
+    It 'Produces the correct members' {
+        $data = Get-Counter -Counter '\Processor(_Total)\% Processor Time' | Expand-DateTime
+        { $data.Timestamp } | Should Not Throw
+        { $data.Value } | Should Not Throw
+        { $data.DayOfWeek } | Should Not Throw
+        { $data.Year } | Should Not Throw
+        { $data.Month } | Should Not Throw
+        { $data.Hour } | Should Not Throw
+    }
 }
