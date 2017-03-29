736d0a49ad36c2accd8a9de48152dea04cc3cf35
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 13:39:52 +0200

    Added property checks

--- Statistics/Get-WeightedValue.ps1
+++ Statistics/Get-WeightedValue.ps1
@@ -19,6 +19,13 @@ function Get-WeightedValue {
 
     Process {
         $InputObject | ForEach-Object {
+            if (($_ | Select-Object -ExpandProperty $Property -ErrorAction 'SilentlyContinue') -eq $null) {
+                throw ('[{0}] Unable to find property <{1}> in input object' -f $MyInvocation.MyCommand, $Property)
+            }
+            if (($_ | Select-Object -ExpandProperty $WeightProperty -ErrorAction 'SilentlyContinue') -eq $null) {
+                throw ('[{0}] Unable to find weight property <{1}> in input object' -f $MyInvocation.MyCommand, $WeightProperty)
+            }
+
             [pscustomobject]@{
                 Value = $_.$WeightProperty * $_.$Property
             }
