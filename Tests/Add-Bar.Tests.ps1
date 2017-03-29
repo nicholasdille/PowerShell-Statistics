d9ef0c4928b2e9fadafc35061f5ed578e4704d0b
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 13:40:10 +0200

    Fix for negative values in property WorkingSet

--- Tests/Add-Bar.Tests.ps1
+++ Tests/Add-Bar.Tests.ps1
@@ -2,26 +2,26 @@ Import-Module -Name Statistics -Force
 
 Describe 'Add-Bar' {
     It 'Produces bars from parameter' {
-        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet
-        $bars = Add-Bar -InputObject $data -Property WorkingSet -Width 50
+        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet64
+        $bars = Add-Bar -InputObject $data -Property WorkingSet64 -Width 50
         $bars | ForEach-Object {
             $_.PSObject.Properties | Where-Object Name -eq 'Bar' | Select-Object -ExpandProperty Name | Should Be 'Bar'
         }
     }
     It 'Produces bars from pipeline' {
-        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet
-        $bars = $data | Add-Bar -Property WorkingSet -Width 50
+        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet64
+        $bars = $data | Add-Bar -Property WorkingSet64 -Width 50
         $bars | ForEach-Object {
             $_.PSObject.Properties | Where-Object Name -eq 'Bar' | Select-Object -ExpandProperty Name | Should Be 'Bar'
         }
     }
     It 'Produces output type [HistogramBucket]' {
-        $item = Get-Process | Add-Bar -Property WorkingSet -Width 50 | Select-Object -First 1
+        $item = Get-Process | Add-Bar -Property WorkingSet64 -Width 50 | Select-Object -First 1
         $item.PSTypeNames -contains 'HistogramBar' | Should Be $true
     }
     It 'Has one bar of maximum width' {
-        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet
-        $bars = Add-Bar -InputObject $data -Property WorkingSet -Width 50
+        $data = Get-Process | Select-Object -Property Name,Id,WorkingSet64
+        $bars = Add-Bar -InputObject $data -Property WorkingSet64 -Width 50
         $bars | Where-Object { $_.Bar.Length -eq 50 } | Should Not Be $null
     }
     It 'Dies on missing property' {
