1318bb6a6985c89b21010827b2e14ec7979e3b1a
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 12:04:01 +0200

    Added test for Get-SlidingAverage

--- Tests/Get-SlidingAverage.Tests.ps1
+++ Tests/Get-SlidingAverage.Tests.ps1
@@ -1,5 +1,9 @@
 Import-Module -Name Statistics -Force
 
 Describe 'Get-SlidingAverage' {
-    It '' {}
+    It 'Produces output' {
+        $data = @(Get-Counter -Counter '\Processor(_Total)\% Processor Time' -SampleInterval 1 -MaxSamples 6 | ConvertFrom-PerformanceCounter -Instance _total | Get-SlidingAverage -Property Value -Size 5)
+        $data -is [array] | Should Be $true
+        $data.Length | Should Be 2
+    }
 }
