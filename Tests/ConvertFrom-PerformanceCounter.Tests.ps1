2fc758f09281ba600a47e004abe2d20ddbfd09d3
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 11:46:31 +0200

    Added tests for ConvertFrom-PerformanceCounter

--- Tests/ConvertFrom-PerformanceCounter.Tests.ps1
+++ Tests/ConvertFrom-PerformanceCounter.Tests.ps1
@@ -1,5 +1,9 @@
 Import-Module -Name Statistics -Force
 
 Describe 'ConvertFrom-PerformanceCounter' {
-    It '' {}
+    It 'Produces the correct properties' {
+        $data = Get-Counter -Counter '\processor(_total)\% processor time' | ConvertFrom-PerformanceCounter -Instance _total
+        { $data.Timestamp } | Should Not Throw
+        { $data.Value } | Should Not Throw
+    }
 }
