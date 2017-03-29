8a3e5b6b68ce27f407901bbc40735050b7f855b1
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 11:53:36 +0200

    Added tests for Get-InterarrivalTime

--- Tests/Get-InterarrivalTime.Tests.ps1
+++ Tests/Get-InterarrivalTime.Tests.ps1
@@ -1,5 +1,15 @@
 Import-Module -Name Statistics -Force
 
 Describe 'Get-InterarrivalTime' {
-    It '' {}
+    $now = Get-Date
+    $data = 1..10 | ForEach-Object {
+        [pscustomobject]@{
+            Timestamp = Get-Random -Minimum $now.AddDays(-1).Ticks -Maximum $now.Ticks
+            Value     = Get-Random -Minimum 0 -Maximum 100
+        }
+    } | Sort-Object -Property Timestamp
+    It 'Produces the correct members' {
+        $data = $data | Get-InterarrivalTime -Property Timestamp
+        { $data.InterarrivalTicks } | Should Not Throw
+    }
 }
