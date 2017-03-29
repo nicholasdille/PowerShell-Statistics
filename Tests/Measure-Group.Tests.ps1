5d8de8b67cf79daeb515f0102f95d64bc2511443
Nicholas Dille <nicholas.dille@mailbox.org>
2017-03-29 12:15:19 +0200

    Added tests for Measure-Group

--- Tests/Measure-Group.Tests.ps1
+++ Tests/Measure-Group.Tests.ps1
@@ -1,5 +1,15 @@
 Import-Module -Name Statistics -Force
 
 Describe 'Measure-Group' {
-    It '' {}
+    It 'Produces correct members' {
+        $now = Get-Date
+        $data = 1..10 | ForEach-Object {
+            [pscustomobject]@{
+                Timestamp = Get-Random -Minimum $now.AddDays(-1).Ticks -Maximum $now.Ticks
+                Value     = Get-Random -Minimum 0 -Maximum 100
+            }
+        } | Sort-Object -Property Timestamp
+        $group = $data | Expand-DateTime | Group-Object -Property Hour | Measure-Group -Property Value
+        { $group.Average } | Should Not Throw
+    }
 }
