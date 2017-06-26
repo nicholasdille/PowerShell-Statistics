fcf8a3a9a66f5b99b26fffa3596a111b36987753
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-06-26 16:09:59 +0200

    Added connectivity check and fixed project directory

--- Build/Start-Build.ps1
+++ Build/Start-Build.ps1
@@ -2,18 +2,21 @@
     $Task = 'Default'
 )
 
-# Install dependencies
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-@('psake', 'PSDeploy', 'BuildHelpers', 'Pester', 'platyps') | ForEach-Object {
-    if (-Not (Get-Module -ListAvailable -Name $_)) {
-        Install-Module -Name $_ -Scope CurrentUser -Force
+# Check connectivity
+if (Get-NetAdapter | Where-Object Status -ieq 'Connected') {
+    # Install dependencies
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+    @('psake', 'PSDeploy', 'BuildHelpers', 'Pester', 'platyps') | ForEach-Object {
+        if (-Not (Get-Module -ListAvailable -Name $_)) {
+            Install-Module -Name $_ -Scope CurrentUser -Force
+        }
     }
 }
 
 # Prepare build environment
 $BHVariables = Get-BuildVariables
 $env:BHBuildSystem   = $BHVariables.BuildSystem
-$env:BHProjectPath   = $BHVariables.ProjectPath
+$env:BHProjectPath   = Get-Item -Path "$PSScriptRoot\.." | Select-Object -ExpandProperty FullName
 $env:BHBranchName    = $BHVariables.BranchName
 $env:BHCommitMessage = $BHVariables.CommitMessage
 $env:BHBuildNumber   = $BHVariables.BuildNumber
