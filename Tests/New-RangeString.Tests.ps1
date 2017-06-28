58681e8dc9c61f626650eaeb348246e5a8aee7a4
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-06-28 16:48:40 +0200

    Fixed scope of tests

--- Tests/New-RangeString.Tests.ps1
+++ Tests/New-RangeString.Tests.ps1
@@ -3,24 +3,22 @@ Get-ChildItem -Path "$env:BHModulePath" -Filter '*.ps1' -File | ForEach-Object {
 }
 
 Describe 'New-RangeString' {
-    InModuleScope Statistics {
-        It 'Works with both indexes in bounds' {
-            New-RangeString -Width 10 -LeftIndex  2 -RightIndex  8 | Should Be ' |-----|'
-        }
-        It 'Works with left index out of bounds' {
-            New-RangeString -Width 10 -LeftIndex -2 -RightIndex  8 | Should Be '<------|'
-        }
-        It 'Works with both right index out of bounds' {
-            New-RangeString -Width 10 -LeftIndex  2 -RightIndex 12 | Should Be ' |------->'
-        }
-        It 'Works with both indexes out of bounds' {
-            New-RangeString -Width 10 -LeftIndex -2 -RightIndex 12 | Should Be '<-------->'
-        }
-        It 'Works with identical indexes' {
-            New-RangeString -Width 10 -LeftIndex  2 -RightIndex  2 | Should Be ' ||'
-        }
-        It 'Works with zero indexes' {
-            New-RangeString -Width 10 -LeftIndex  0 -RightIndex  0 | Should Be '||'
-        }
+    It 'Works with both indexes in bounds' {
+        New-RangeString -Width 10 -LeftIndex  2 -RightIndex  8 | Should Be ' |-----|'
+    }
+    It 'Works with left index out of bounds' {
+        New-RangeString -Width 10 -LeftIndex -2 -RightIndex  8 | Should Be '<------|'
+    }
+    It 'Works with both right index out of bounds' {
+        New-RangeString -Width 10 -LeftIndex  2 -RightIndex 12 | Should Be ' |------->'
+    }
+    It 'Works with both indexes out of bounds' {
+        New-RangeString -Width 10 -LeftIndex -2 -RightIndex 12 | Should Be '<-------->'
+    }
+    It 'Works with identical indexes' {
+        New-RangeString -Width 10 -LeftIndex  2 -RightIndex  2 | Should Be ' ||'
+    }
+    It 'Works with zero indexes' {
+        New-RangeString -Width 10 -LeftIndex  0 -RightIndex  0 | Should Be '||'
     }
 }
